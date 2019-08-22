66b7e4bf478ebcfd759850927dad990145b367d6
Dennis Schad <dschad@comwrap.com>
2019-08-22 17:15:05 +0200

    refactored to render with opts append and prepend

--- src/sightly.js
+++ src/sightly.js
@@ -3,17 +3,19 @@
 const sightly = require('@adobe/htlengine/src/main')
 
 /**
- * Transform Sightly to HTML.
+ * Transform Sightly/HTL to HTML.
  * @public
  * @param {String} template - Path to the Sightly/HTL file being rendered.
  * @param {Object} resource - JSON data used to render the file.
  * @returns {Promise<String>} HTML.
  */
 
-module.exports = async function(template, resource) {
+module.exports = async function (template, resource, opts) {
 
-	const res = await sightly(resource, template)
-
-	return res
+	return sightly(resource, `
+		${ typeof opts.prepend === 'string' ? opts.prepend : ''}
+		${ template}
+		${ typeof opts.append === 'string' ? opts.append : ''}
+	`)
 
 }
